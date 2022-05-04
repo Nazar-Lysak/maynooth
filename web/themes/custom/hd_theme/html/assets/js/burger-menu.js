c34f0a0a74b9f9acc3d05f364076fb52c35c0252
Lysak <Nazarii.Lysak@ua.nestle.com>
2022-05-04 10:37:47 +0300

    isues with burger.js fixed

---
+++ web/themes/custom/hd_theme/html/assets/js/burger-menu.js
@@ -0,0 +1,16 @@
+(function ($, Drupal, once) {
+
+
+    Drupal.behaviors.indexBehavior = {
+      attach: function (context, settings) {
+        const burgerMenu = document.querySelector('.burger-menu');
+        const mainMenu = document.querySelector('.main-menu');
+
+        burgerMenu.addEventListener('click', () => {
+          burgerMenu.classList.toggle("active");
+          mainMenu.classList.toggle("active");
+        })
+      }
+    };
+
+  })(jQuery, Drupal, once);
