3e82125145c6e2ee7633a931e5e587997ae6be88
Lysak <Nazarii.Lysak@ua.nestle.com>
2022-05-05 13:48:12 +0300

    response livingroom, fixed bugs

--- web/themes/custom/hd_theme/html/assets/js/burger-menu.js
+++ web/themes/custom/hd_theme/html/assets/js/burger-menu.js
@@ -1,16 +1,15 @@
 (function ($, Drupal, once) {
 
+  Drupal.behaviors.burgerBehavior = {
+    attach: function (context, settings) {
+      const burgerMenu = document.querySelector('.burger-menu');
+      const mainMenu = document.querySelector('.main-menu');
 
-    Drupal.behaviors.indexBehavior = {
-      attach: function (context, settings) {
-        const burgerMenu = document.querySelector('.burger-menu');
-        const mainMenu = document.querySelector('.main-menu');
+      burgerMenu.addEventListener('click', () => {
+        burgerMenu.classList.toggle("active");
+        mainMenu.classList.toggle("active");
+      })
+    }
+  };
 
-        burgerMenu.addEventListener('click', () => {
-          burgerMenu.classList.toggle("active");
-          mainMenu.classList.toggle("active");
-        })
-      }
-    };
-
-  })(jQuery, Drupal, once);
+})(jQuery, Drupal, once);
