6966268472ef21b6bb4b52c601500bceb2578956
Lysak <Nazarii.Lysak@ua.nestle.com>
2022-05-08 23:59:46 +0300

    prod response

--- web/themes/custom/hd_theme/html/assets/js/index.js
+++ web/themes/custom/hd_theme/html/assets/js/index.js
@@ -1,17 +1,5 @@
 (function ($, Drupal, once) {
 
-  Drupal.behaviors.productTabsBehavior = {
-    attach: function (context, settings) {
-      $(function () {
-        $('.tabs__buttons').on('click', 'div:not(.field-tab__item_active)', function () {
-          $(this).addClass('field-tab__item_active').siblings().removeClass('field-tab__item_active')
-            .closest('div.tabs').find('div.field-content__item').removeClass('field-content__item_active')
-            .eq($(this).index()).addClass('field-content__item_active');
-        })
-      })
-    }
-  };
-
   Drupal.behaviors.blogRenderBehavior = {
     attach: function (context, settings) {
       const cont = document.querySelectorAll('.views-row')
