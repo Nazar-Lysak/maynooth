a96073d5aeb5907d825f0e3e8b36c75e580443f0
Lysak <Nazarii.Lysak@ua.nestle.com>
2022-05-08 20:04:51 +0300

    blog listing page has been created

--- web/themes/custom/hd_theme/html/assets/js/index.js
+++ web/themes/custom/hd_theme/html/assets/js/index.js
@@ -12,4 +12,15 @@
     }
   };
 
+  Drupal.behaviors.blogRenderBehavior = {
+    attach: function (context, settings) {
+      const cont = document.querySelectorAll('.views-row')
+      $(function () {
+        cont.forEach((el, i) => i % 2 == 0 ? el.classList.add('first-row') : el);
+      })
+    }
+  };
+
 })(jQuery, Drupal, once);
+
+
